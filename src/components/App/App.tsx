dd7fb6f0640915eab278954b73f63be3e9106d47
Mike Flynn <mdflynn34@outlook.com>
2021-08-26 17:07:06 -0600

    add func to pass movie poster

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,13 +5,16 @@ import HomePage from "../HomePage/HomePage";
 import MoviePage from "../MoviePage/MoviePage";
 import { Route, Switch } from "react-router-dom";
 
-function App() {
+const App: React.FC = () => {
+  const [selectedPoster, setSelectedPoster] = useState('');
+
   return (
     <Switch>
-      <Route exact path="/" component={HomePage} />
+      <Route exact path="/" render={() => <HomePage poster={setSelectedPoster}/>} />
       <Route
         path="/movie/:id"
         render={({ match }) => <MoviePage id={match.params.id} />}
+        // add selectedPoster in the render
       />
     </Switch>
   );
