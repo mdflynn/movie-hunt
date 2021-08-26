5b4bbe6ec629870eb4a48569c3996c59225415f6
Mike Flynn <mdflynn34@outlook.com>
2021-08-25 19:07:57 -0600

    update Movie interface

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,19 +1,21 @@
 interface Movie {
-  id: string;
-  title: string;
-  genres: string[];
+  data: {
+    id: string;
+    title: string;
+    genres: string[];
+  };
 }
 
 export const getAllMovies = async (): Promise<Movie> => {
-    const response = await fetch(
-      "https://code-challenge.spectrumtoolbox.com/api/movies",
-  
-      {
-        headers: {
-          authorization: "Api-Key q3MNxtfep8Gt",
-        },
-      }
-    );
-  
-    return response.json();
-  };
+  const response = await fetch(
+    "https://code-challenge.spectrumtoolbox.com/api/movies",
+
+    {
+      headers: {
+        authorization: "Api-Key q3MNxtfep8Gt",
+      },
+    }
+  );
+
+  return response.json();
+};
