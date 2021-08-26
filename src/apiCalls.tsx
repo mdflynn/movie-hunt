de7756229efac4c7d99c04ee210d74433a6f0852
Mike Flynn <mdflynn34@outlook.com>
2021-08-25 18:59:21 -0600

    add fetch for all movies

---
+++ src/apiCalls.tsx
@@ -0,0 +1,19 @@
+interface Movie {
+  id: string;
+  title: string;
+  genres: string[];
+}
+
+export const getAllMovies = async (): Promise<Movie> => {
+    const response = await fetch(
+      "https://code-challenge.spectrumtoolbox.com/api/movies",
+  
+      {
+        headers: {
+          authorization: "Api-Key q3MNxtfep8Gt",
+        },
+      }
+    );
+  
+    return response.json();
+  };
