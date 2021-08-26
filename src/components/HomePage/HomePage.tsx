524a48e2d68591d5caa8f2527a25592bb5a11f3c
Mike Flynn <mdflynn34@outlook.com>
2021-08-25 19:08:11 -0600

    add useEffect to log fetch data

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import "./HomePage.css"
+import React, { useEffect } from "react";
+import "./HomePage.css";
+
+import { getAllMovies } from "../../apiCalls";
 
 const HomePage: React.FC = () => {
-    return (
-        <h1>Home page</h1>
-    )
-}
+  useEffect(() => {
+    getAllMovies().then((data) => console.log(data.data));
+  });
+
+  return <h1>Home page</h1>;
+};
 
 export default HomePage;
