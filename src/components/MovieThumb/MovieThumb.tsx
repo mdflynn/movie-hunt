edc228d5ddbf5d6decf3f8cde56674513de31cb4
Mike Flynn <mdflynn34@outlook.com>
2021-08-26 17:09:20 -0600

    adds type for prop passing to child comp

--- src/components/MovieThumb/MovieThumb.tsx
+++ src/components/MovieThumb/MovieThumb.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import "./MovieThumb.css";
 import { Link } from "react-router-dom";
 
@@ -15,22 +15,24 @@ function importAll(r: any) {
 interface Props {
   key: number;
   movieData: Movie;
+  poster: Dispatch<SetStateAction<any>>;
 }
-
+// make dynamic for MovieThumb and Movie
 const findMovieThumbImage = (id: string) => {
   return movieThumbImages.find((movie: any) => movie.default.includes(id));
 };
 
-const MovieThumb: React.FC<Props> = ({ movieData }) => {
+const MovieThumb: React.FC<Props> = ({ movieData, poster }) => {
   const getImage = findMovieThumbImage(movieData.id);
+  // move image import to App to pass down
 
   const movieImage =
     getImage === undefined ? movieThumbImages[33].default : getImage.default;
 
   return (
     <Link to={`movie/${movieData.id}`}>
-      <article className="thumb-article">
-        <img className="thumb-img" src={movieImage} />
+      <article className="thumb-article" onClick={() => poster(movieImage)}>
+        <img className="thumb-img" src={movieImage} alt={movieData.title} />
         <h1 className="thumb-title">{movieData.title}</h1>
       </article>
     </Link>
