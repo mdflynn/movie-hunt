e7b744b9dd373a4a4e74e8082065fd10e9f79d36
Mike Flynn <mdflynn34@outlook.com>
2021-08-26 12:00:10 -0600

    adds method to find moviethumb image

--- src/components/MovieThumb/MovieThumb.tsx
+++ src/components/MovieThumb/MovieThumb.tsx
@@ -4,19 +4,30 @@ import "./MovieThumb.css";
 import { Movie } from "../../apiCalls";
 
 function importAll(r: any) {
-    return r.keys().map(r);
-  }
-  
-const movieThumbImages = importAll(require.context('../../assets/moviePosterImages', false, /\.(png|jpe?g|svg)$/));
+  return r.keys().map(r);
+}
 
+const movieThumbImages = importAll(
+  require.context("../../assets/moviePosterImages", false, /\.(png|jpe?g|svg)$/)
+);
 
 interface Props {
   key: number;
   movieData: Movie;
 }
 
+
+const findMovieThumbImage = (id: string) => {
+  return movieThumbImages.find((movie: any) => movie.default.includes(id)).default;
+};
+
 const MovieThumb: React.FC<Props> = ({ movieData }) => {
-  return <img src={movieThumbImages[1].default} />
+  return (
+    <article>
+      <img src={findMovieThumbImage(movieData.id)} />
+      <h1>{movieData.title}</h1>
+    </article>
+  );
 };
 
 export default MovieThumb;
