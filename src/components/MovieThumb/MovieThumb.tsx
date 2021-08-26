d65139bea4b308354e830a04ca854e2ee85c286d
Mike Flynn <mdflynn34@outlook.com>
2021-08-26 11:44:34 -0600

    add func to import images

--- src/components/MovieThumb/MovieThumb.tsx
+++ src/components/MovieThumb/MovieThumb.tsx
@@ -3,13 +3,20 @@ import "./MovieThumb.css";
 
 import { Movie } from "../../apiCalls";
 
+function importAll(r: any) {
+    return r.keys().map(r);
+  }
+  
+const movieThumbImages = importAll(require.context('../../assets/moviePosterImages', false, /\.(png|jpe?g|svg)$/));
+
+
 interface Props {
   key: number;
   movieData: Movie;
 }
 
 const MovieThumb: React.FC<Props> = ({ movieData }) => {
-  return <h1>MovieThumb</h1>;
+  return <img src={movieThumbImages[1].default} />
 };
 
 export default MovieThumb;
