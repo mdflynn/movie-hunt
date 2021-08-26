c7efead649dc7b1e9f869f46841a17ae32ee8dba
Mike Flynn <mdflynn34@outlook.com>
2021-08-26 12:15:45 -0600

    updates method for default image

--- src/components/MovieThumb/MovieThumb.tsx
+++ src/components/MovieThumb/MovieThumb.tsx
@@ -8,23 +8,27 @@ const movieThumbImages = importAll(
 );
 
 function importAll(r: any) {
-    return r.keys().map(r);
-  }
+  return r.keys().map(r);
+}
 
 interface Props {
   key: number;
   movieData: Movie;
 }
 
-
 const findMovieThumbImage = (id: string) => {
-  return movieThumbImages.find((movie: any) => movie.default.includes(id)).default;
+  return movieThumbImages.find((movie: any) => movie.default.includes(id));
 };
 
 const MovieThumb: React.FC<Props> = ({ movieData }) => {
+  const getImage = findMovieThumbImage(movieData.id);
+
+  const movieImage =
+    getImage === undefined ? movieThumbImages[33] : getImage.default;
+
   return (
     <article className="thumb-article">
-      <img className="thumb-img" src={findMovieThumbImage(movieData.id)} />
+      <img className="thumb-img" src={movieImage} />
       <h1 className="thumb-title">{movieData.title}</h1>
     </article>
   );
