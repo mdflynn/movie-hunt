9c0e65711b50b4769bbc2f8317f1eca72b478cab
Mike Flynn <mdflynn34@outlook.com>
2021-08-29 14:19:11 -0600

    add boilerplate to Nav

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav: React.FC = () => {
+    return (
+        <nav className="nav">
+           <p className="nav-title">Movie Hunt</p> 
+        </nav>
+    )
+}
+
+export default Nav;
