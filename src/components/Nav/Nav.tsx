6beda62d349da4b49347243382e434843526452c
Mike Flynn <mdflynn34@outlook.com>
2021-08-29 15:00:59 -0600

    pass searchCritieria to SearchBar

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import "./Nav.css";
 
 import SearchBar from "../SearchBar/SearchBar";
 
-const Nav: React.FC = () => {
+const Nav: React.FC<{
+  search: Dispatch<SetStateAction<any>>;
+  criteria: string;
+}> = ({ search, criteria }) => {
   return (
     <nav className="nav">
-      <SearchBar />
+      <SearchBar search={search} criteria={criteria}/>
       <p className="nav-title">Movie Hunt</p>
     </nav>
   );
