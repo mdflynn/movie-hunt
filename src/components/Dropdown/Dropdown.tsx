4a8f4be89d37d4f6a71c3be92eadd90de1c804ac
Mike Flynn <mdflynn34@outlook.com>
2021-08-29 17:21:05 -0600

    remove unused import

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState, Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 
 const Dropdown: React.FC<{ filter: Dispatch<SetStateAction<any>> }> = ({
   filter,
