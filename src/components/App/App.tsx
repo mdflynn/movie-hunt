96fa6b6f6479f5b8f076dab5017b8837a2739c4a
Mike Flynn <mdflynn34@outlook.com>
2021-08-29 14:19:37 -0600

    import Nav to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,21 +3,29 @@ import "./App.css";
 
 import HomePage from "../HomePage/HomePage";
 import MoviePage from "../MoviePage/MoviePage";
+import Nav from "../Nav/Nav";
 import { Route, Switch } from "react-router-dom";
 
 const App: React.FC = () => {
-  const [selectedPoster, setSelectedPoster] = useState('');
+  const [selectedPoster, setSelectedPoster] = useState("");
 
   return (
-    <Switch>
-      <Route exact path="/" render={() => <HomePage poster={setSelectedPoster}/>} />
-      <Route
-        path="/movie/:id"
-        render={({ match }) => <MoviePage id={match.params.id} />}
-        // add selectedPoster in the render
-      />
-    </Switch>
+    <>
+      <Nav />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => <HomePage poster={setSelectedPoster} />}
+        />
+        <Route
+          path="/movie/:id"
+          render={({ match }) => <MoviePage id={match.params.id} />}
+          // add selectedPoster in the render
+        />
+      </Switch>
+    </>
   );
-}
+};
 
 export default App;
