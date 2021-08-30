62a47aa4000d942a1946b24f8b3fe5488ff2a82e
Mike Flynn <64620860+mdflynn@users.noreply.github.com>
2021-08-30 08:03:02 -0600

    Update index.tsx
    Add @ts-ignore for react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+// @ts-ignore
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
